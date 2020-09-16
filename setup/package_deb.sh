0a09c7ce05fd59dd8cb3cffaf28cc0c4bfb7d94b
hoaivan <hoaivan@gmail.com>
2020-09-16 18:01:40 +0700

    Delete package_deb.sh

--- setup/package_deb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python package.py --build-deb --build-dir /home/voi/Projects/SchoolERP/odoo-build/ --pub /home/voi/Projects/SchoolERP/odoo-public/
-echo "Fail dpkg-scanpackages is OK. Just grab deb file from pub dir"
